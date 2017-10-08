974b4b5f5e162565e5c41e8752c3912104bd00f0
IceMimosa <chk@terminus.io>
2017-10-08 23:02:31 +0800

    Add some init

---
+++ app/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <div class="header">
+
+    </div>
+  </header>
+</template>
+
+
+<style lang="scss" scoped>
+.header {
+  height: 50px;
+  border-bottom: 1px solid #E8E8E8;
+  background: white;
+}
+</style>
---
+++ bin/server.js
@@ -0,0 +1,22 @@
+const { Nuxt, Builder } = require('nuxt')
+const app = require('express')()
+const port = process.env.PORT || 3000
+
+// We instantiate Nuxt.js with the options
+let config = require('../nuxt.config.js')
+const nuxt = new Nuxt(config)
+app.use(nuxt.render)
+
+// Build only in dev mode
+if (config.dev) {
+  new Builder(nuxt).build()
+    .catch((error) => {
+      console.error(error)
+      process.exit(1)
+    })
+}
+
+// Listen the server
+app.listen(port, '0.0.0.0', () => {
+  nuxt.showOpen()
+})

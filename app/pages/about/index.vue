2be54768a2fdfdc6ac6320993f3bd0586ae1fa89
IceMimosa <chk@terminus.io>
2017-12-19 09:13:02 +0800

    Add about

---
+++ app/pages/about/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="about">
+    about
+  </div>
+</template>
+
+<script>
+export default {
+  fetch ({ params }) {
+    // const id = params.id
+  }
+}
+</script>
+
+<style lang="scss">
+#about {
+  margin: 0 auto;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  background: #F2F3F5;
+  padding-top: 80px;
+}
+
+</style>
